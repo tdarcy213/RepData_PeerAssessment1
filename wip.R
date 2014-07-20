ae720547b0b2088dcc5512a115248f3afa1f8919
tdarcy213 <tgdarcy@gmail.com>
2014-07-20 19:57:22 -0400

    Final Repository
    Completed files for assignment 1.

--- wip.R
+++ wip.R
@@ -3,12 +3,22 @@ activity_data<-read.csv("activity.csv",as.is="date")
 summary(activity_data)
 activity_data$date<-as.Date(activity_data$date)
 complete_data<-data.table(activity_data[complete.cases(activity_data$steps),])
+complete_data$dayofweek<-factor(weekdays(complete_data$date))
+
 incomplete_data<-data.table(activity_data[!complete.cases(activity_data$steps),])
+incomplete_data$dayofweek<-factor(weekdays(incomplete_data$date))
+
 complete_data_summarized<-complete_data[,list(sum_steps=sum(steps),mean_steps=mean(steps),med_steps=median(steps)),by=list(date,interval)]
 complete_data_summarized$dayofweek<-factor(ifelse(weekdays(complete_data_summarized$date)%in%c('Saturday','Sunday'),"weekend","weekday"))
 with(complete_data_summarized,plot(date,sum_steps,type="h",col="blue",lwd=4,xlab="Date",ylab="Total Steps"))
 windows()
 library(lattice)
 with(complete_data_summarized,xyplot(mean_steps~interval|dayofweek,type="l",layout=c(1,2),xlab="Interval",ylab="Number of Steps"))
+impute_values<-complete_data[,list(mean_steps=mean(steps)),by=list(dayofweek,interval)]
+imputed_data<-merge(incomplete_data,impute_values,by=c("dayofweek","interval"))
+imputed_data2<-subset(imputed_data,select=c("mean_steps","date","interval","dayofweek"))
+colnames(imputed_data2)<-(c("steps","date","interval","dayofweek"))
+all_data<-rbind(imputed_data2,complete_data)
+with(all_data_summarized,xyplot(mean_steps~interval|daytype,type="l",layout=c(1,2),xlab="Interval",ylab="Number of Steps"))
 
 
--- wip2.R
+++ wip2.R
@@ -1,50 +1,25 @@
----
-title: "PA1_template"
-output: html_document
----
-
-# Reproducible Research: Peer Assessment 1
-
-
-## Loading and preprocessing the data
-```{r}
 library(data.table)
 library(lattice)
 activity_data<-read.csv("activity.csv",as.is="date")
 activity_data$date<-as.Date(activity_data$date)
 ## create data tables with & without NAs
 complete_data<-data.table(activity_data[complete.cases(activity_data$steps),])
-complete_data$dayofweek<-factor(weekdays(complete_data$date))
 incomplete_data<-data.table(activity_data[!complete.cases(activity_data$steps),])
-incomplete_data$dayofweek<-factor(weekdays(incomplete_data$date))
-```
-
-
-## What is mean total number of steps taken per day?
-```{r}
+complete_data$dayofweek<-factor(ifelse(weekdays(complete_data$date)%in%c('Saturday','Sunday'),"weekend","weekday"))
 ## create histogram of total steps per day; calculate mean and median
 stepsBydate<-complete_data[,list(sum_steps=sum(steps)),by=date]
+windows()
 with(stepsBydate,plot(date,sum_steps,type="h",col="blue",lwd=4,xlab="Date",ylab="Total Steps"))
 meanSteps<-mean(stepsBydate$sum_steps)
 medSteps<-median(stepsBydate$sum_steps)
-```
-The mean total of steps is `r meanSteps`; median is `r medSteps`
-
-## What is the average daily activity pattern?
-```{r}
 ## create line plot of average steps per time interval
 stepsByinterval<-complete_data[,list(avg_steps=mean(steps)),by=interval]
+windows()
 with(stepsByinterval,plot(interval,avg_steps,type="l",col="blue",xlab="Interval",ylab="Average Steps"))
-maxSteps<-stepsByinterval[avg_steps==max(avg_steps),]
-```
-The maximum average is `r maxSteps$avg_steps` occurring at interval `r maxSteps$interval`.
-
-## Imputing missing values
-
-```{r}
+stepsByinterval[avg_steps==max(avg_steps),]
 ## how many NAs?
-numNAs<-nrow(incomplete_data)
-
+nrow(incomplete_data)
+## We will impute 
 impute_values<-complete_data[,list(mean_steps=mean(steps)),by=list(dayofweek,interval)]
 imputed_data<-merge(incomplete_data,impute_values,by=c("dayofweek","interval"))
 imputed_data2<-subset(imputed_data,select=c("mean_steps","date","interval","dayofweek"))
@@ -56,20 +31,7 @@ with(allstepsBydate,plot(date,sum_steps,type="h",col="blue",lwd=4,xlab="Date",yl
 
 allmeanSteps<-mean(allstepsBydate$sum_steps)
 allmedSteps<-median(allstepsBydate$sum_steps)
-
-```
-
-There are `r numNAs` incomplete rows.
-
-The "imputed" mean is `r allmeanSteps`; the "imputed" median is `r allmedSteps`.  The difference is minimal.
-
-
-## Are there differences in activity patterns between weekdays and weekends?
-```{r}
-
+with(all_data,xyplot(mean_steps~interval|dayofweek,type="l",layout=c(1,2),xlab="Interval",ylab="Number of Steps"))
 all_data_summarized<-all_data[,list(mean_steps=mean(steps)),by=list(date,interval)]
 all_data_summarized$daytype<-factor(ifelse(weekdays(all_data_summarized$date)%in%c('Saturday','Sunday'),"weekend","weekday"))
 with(all_data_summarized,xyplot(mean_steps~interval|daytype,type="l",layout=c(1,2),xlab="Interval",ylab="Number of Steps"))
-```
-
-The plot shows that the greater number of steps were recorded later in the day on weekends, as opposed to weekdays, when the greatest number of steps occurred earlier in the day.
